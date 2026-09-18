1f80d0d869a142e8c637affe80b8a2b82b17a3c4
Marcos Fellipe Silva de Oliveira <marcosf1625@gmail.com>
2026-09-18 20:46:44 -0300

    Create MonteCarloAgent.py

---
+++ BlackJack/MonteCarloAgent.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+from EpsilonGreedy import EpsilonGreedy
+
+class MonteCarlo:
+    def __init__(self,env,state_size,action_size,seed,learning_rate,epsilon,gamma):
+        self.env = env
+        self.action_size = action_size
+        self.state_size = state_size
+        self.seed = seed
+
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
+        self.gamma = gamma
+
+        self.epsilongreedy = EpsilonGreedy(
+            seed=self.seed
+            ,epsilon=self.epsilon
+        )
+
+        self.reset_qtable()
+
+    def reset_qtable(self):
+        self.qtable = np.zeros(
+            self.state_size + (self.action_size,)
+            )
+
+    def train(self, qtable,episode):
+        G = 0
+
+        for state ,action ,reward in reversed(episode):
+            G = reward + self.gamma * G
+
+            qtable[state,action] = (
+                qtable[state,action] + self.learning_rate * (G - qtable[state,action])
+            )
+
+        return qtable
